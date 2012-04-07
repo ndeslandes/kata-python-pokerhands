7fcfc9f5d787b4a4439b0b4834572b0f4ad2ffdf
Nicolas Deslandes <deslandes.nicolas@gmail.com>
2012-04-06 23:53:37 -0400

    full detection

--- poker_solver.py
+++ poker_solver.py
@@ -3,53 +3,66 @@ class PokerSolver(object):
         pass
         
     cards = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
-    hands = {'nothing':None, 'high_card':1, 'pair':2, 'two_pairs':3, 'three_of_a_kind':4, 'straight':5, 'flush':6, 'full_house':7, 'four_of_a_kind':8, 'straight_flush':9}
+    sets = ['H', 'S', 'C', 'D']
+    hands = {'high_card':1, 'pair':2, 'two_pairs':3, 'three_of_a_kind':4, 'straight':5, 'flush':6, 'full_house':7, 'four_of_a_kind':8, 'straight_flush':9}
     
-    def solve(self, hand):
-        part = hand.split(' ');
-        black_hand = part[1:6]
-        white_hand = part[8:]
+    def solve(self, hands):
+        hand = hands.split('  ');
+        
+        black_name = hand[0].split(':')[0]
+        white_name = hand[1].split(':')[0]
+
+        black_hand = hand[0].split(':')[1].split(' ')[1:6]
+        white_hand = hand[1].split(':')[1].split(' ')[1:6]
         
         black_score = self._evaluate_hand(black_hand)
         white_score = self._evaluate_hand(white_hand)
         
         if black_score[0] > white_score[0]:
-            return 'black'
+            return black_name
         if white_score[0] > black_score[0]:
-            return 'white'
+            return white_name
         if black_score[0] == white_score[0]:
             if black_score[1] > white_score[1]:
-                return 'black'
+                return black_name
             if white_score[1] > black_score[1]:
-                return 'white'
-            return 'tie'
+                return white_name
+            if black_score[1] == white_score[1]:
+                return 'tie'
     
     def _evaluate_hand(self, hand):
+        hand = self._sort_hand(hand)
+        fifth_card_value = self.cards[hand[4][0]]
+        fourth_card_value = self.cards[hand[3][0]]
+        third_card_value = self.cards[hand[2][0]]
+        second_card_value = self.cards[hand[1][0]]
+        first_card_value = self.cards[hand[0][0]]
+        if self._detect_straight_flush(hand):
+            return (self.hands['straight_flush'], self._detect_straight_flush(hand))
+        if self._detect_four_of_a_kind(hand):
+            return (self.hands['four_of_a_kind'], [self._detect_four_of_a_kind(hand), fifth_card_value])
+        if self._detect_full_house(hand):
+            return (self.hands['full_house'], self._detect_full_house(hand))
+        if self._detect_flush(hand):
+            return (self.hands['flush'], self._detect_flush(hand))
         if self._detect_straight(hand):
             return (self.hands['straight'], self._detect_straight(hand))
         if self._detect_three_of_a_kind(hand):
-            return (self.hands['three_of_a_kind'], self._detect_three_of_a_kind(hand))
+            return (self.hands['three_of_a_kind'], [self._detect_three_of_a_kind(hand), fifth_card_value, fourth_card_value])
         if self._detect_two_pairs(hand):
-            return (self.hands['two_pairs'], self._detect_two_pairs(hand))
+            return (self.hands['two_pairs'], [self._detect_two_pairs(hand), fifth_card_value])
         if self._detect_pair(hand):
-            return (self.hands['pair'], self._detect_pair(hand))
-        if self._detect_high_card(hand):
-            return (self.hands['high_card'], self._detect_high_card(hand))
-        return (self.hands['nothing'], None)
+            return (self.hands['pair'], [self._detect_pair(hand), fifth_card_value, fourth_card_value, third_card_value])
+        return (self.hands['high_card'], [fifth_card_value, fourth_card_value, third_card_value, second_card_value, first_card_value])
     
     def _convert_in_value(self, hand):
         hand_in_value = []
         for card in hand:
             hand_in_value.append(self.cards[card[0]])
         return hand_in_value
-
-    def _detect_high_card(self, hand):
-        hand_in_value = self._convert_in_value(hand)
-        high_card = None
-        for card in hand_in_value:
-            if card > high_card:
-                high_card = card
-        return high_card
+    
+    def _sort_hand(self, hand):
+        return sorted(hand, key=lambda card: self.cards[card[0]])
 
     def _detect_pair(self, hand):
         counter = dict([(card, 0) for card in self.cards.viewkeys()]) 
@@ -84,5 +97,35 @@ class PokerSolver(object):
         hand_in_value.sort()
         if len(hand_in_value) == 5:
             if hand_in_value[4]-hand_in_value[0] == 4:
-                return hand_in_value[4]
+                return [hand_in_value[4]]
+        return None
+
+    def _detect_flush(self, hand):
+        counter = dict([(a_set, 0) for a_set in self.sets])
+        for card in hand:
+            counter[card[1]] += 1
+            if counter[card[1]] == 5:
+                return [self.cards[card[0]]]
+        return None
+    
+    def _detect_full_house(self, hand):
+        pair = self._detect_pair(hand)
+        three_of_a_kind = self._detect_three_of_a_kind(hand)
+        if bool(pair) & bool(three_of_a_kind):
+            return [three_of_a_kind, pair]
+        return None
+
+    def _detect_four_of_a_kind(self, hand):
+        counter = dict([(card, 0) for card in self.cards.keys()]) 
+        for card in hand:
+            counter[card[0]] += 1
+            if counter[card[0]] == 4:
+                return self.cards[card[0]]
+        return None
+
+    def _detect_straight_flush(self, hand):
+        straight = self._detect_straight(hand)
+        flush = self._detect_flush(hand)
+        if bool(straight) & bool(flush):
+            return straight
         return None
--- poker_solver_test.py
+++ poker_solver_test.py
@@ -11,27 +11,49 @@ class PokerSolverTest(unittest.TestCase):
         self.assertEquals(self.solver.solve('Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH'), 'tie')
     
     def test_solve_withHighCard(self):
-        self.assertEquals(self.solver.solve('Black: 2H 3D 5S 9C AD  White: 2D 3H 5C 9S KH'), 'black')
+        self.assertEquals(self.solver.solve('Black: 2H 3D 5S 9C AD  White: 2D 3H 5C 9S KH'), 'Black')
         
     def test_solve_withOnePair(self):
-        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 9C KD  White: 2D 3H 5C 9S KH'), 'black')
-        self.assertEquals(self.solver.solve('Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 5S KH'), 'white')
-        self.assertEquals(self.solver.solve('Black: 2H 3D 5S KC KD  White: 2D 3H 9C 9S KH'), 'black')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 9C KD  White: 2D 3H 5C 9S KH'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 5S KH'), 'White')
+        self.assertEquals(self.solver.solve('Black: 2H 3D 5S KC KD  White: 2D 3H 9C 9S KH'), 'Black')
         self.assertEquals(self.solver.solve('Black: 2H 3D 5S KC KD  White: 2D 3H 5C KS KH'), 'tie')
     
     def test_solve_withTwoPair(self):
-        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 5C KD  White: 2D 3H 5C 9S KH'), 'black')
-        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 5C KD  White: 2D 3H 5C KS KH'), 'black')
-        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 5C KD  White: 2D 5H 5C KS KH'), 'white')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 5C KD  White: 2D 3H 5C 9S KH'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 5C KD  White: 2D 3H 5C KS KH'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 5S 5C KD  White: 2D 5H 5C KS KH'), 'White')
         self.assertEquals(self.solver.solve('Black: 2H 5D 5S KC KD  White: 2D 5H 5C KS KH'), 'tie')
         
     def test_solve_withThreeOfAKind(self):
-        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 9C KD  White: 3D 3H 5C 9S KH'), 'black')
-        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 9C KD  White: 3D 3H 3C 9S KH'), 'white')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 9C KD  White: 3D 3H 5C 9S KH'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 9C KD  White: 3D 3H 3C 9S KH'), 'White')
 
     def test_solve_withStraight(self):
-        self.assertEquals(self.solver.solve('Black: 2H 3D 4S 5C 6D  White: 2D 3H 5C 9S KH'), 'black')
-        self.assertEquals(self.solver.solve('Black: 2H 3D 4S 5C 6D  White: TD JH QC KS AH'), 'white')
+        self.assertEquals(self.solver.solve('Black: 2H 3D 4S 5C 6D  White: 2D 3H 5C 9S KH'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 9H 6D 7S 8C 5D  White: 3D 4H 5C 6S 7H'), 'Black')
+
+    def test_solve_withFlush(self):
+        self.assertEquals(self.solver.solve('Black: 2H 3D 4S 5C 6D  White: 2H 3H 5H 9H KH'), 'White')
+        self.assertEquals(self.solver.solve('Black: 2D 3D KD AD TD  White: 2H 3H QH JH KH'), 'Black')
+
+    def test_solve_withFullHouse(self):
+        self.assertEquals(self.solver.solve('Black: 2H 2D 4S 4C 4D  White: 2H 3H 5H 6H 7H'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 4S 4C 4D  White: 7H 7S 5H 5H 5H'), 'White')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 4S 4C 4D  White: 7H 7S 3H 3H 3H'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 7H 7D 4S 4C 4D  White: 2H 2S 4H 4H 4H'), 'Black')
+
+    def test_solve_withFourOfAKind(self):
+        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 2C KD  White: 3D 3H 5C 9S KH'), 'Black')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 2C KD  White: 3D 3H 3C 3S KH'), 'White')
+        self.assertEquals(self.solver.solve('Black: 2H 2D 2S 2C KD  White: 2D 2H 2C 2S AH'), 'White')
+
+    def test_solve_withStraightFlush(self):
+        self.assertEquals(self.solver.solve('Black: 2H 3D 4S 5C 6D  White: 2H 3H 4H 5H 6H'), 'White')
+        self.assertEquals(self.solver.solve('Black: 7S 8S 9S TS JS  White: 2H 3H 4H 5H 6H'), 'Black')
+        
+    def test_solve_withOtherName(self):
+        self.assertEquals(self.solver.solve('Nicolas: 2H 3D 4S 5C 6D  Sebastien: 2H 3H 4H 5H 6H'), 'Sebastien')
 
 if __name__ == '__main__':
     unittest.main()
